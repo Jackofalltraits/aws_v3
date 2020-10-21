cd608fced730c5beaea6d995873abb7d458e0561
jackkower <jackkower@gmail.com>
2020-10-21 02:08:43 -0700

    delta3

--- src/App.js
+++ src/App.js
@@ -1,41 +1,12 @@
-import React from "react";
-import { Route, BrowserRouter } from "react-router-dom";
-import Home from "./Home";
-// import About from "./About";
-// import Contact from './Contact'
-// import Checkout from './Checkout'
-// import Products from "./Products";
-// import Beanies from "./Beanies";
-// import Blankets from "./Blankets";
-// import Scarves from "./Scarves";
-// import Coasters from "./Coasters";
-// import Jewelry from "./Jewelry";
-// import Kandi from "./Kandi";
-// import Magnets from "./Magnets";
-
-import {  } from "react-bootstrap";
-
+import React from 'react';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <header className="App-header">
 
-           <BrowserRouter>
-     <Route path='/' exact component={Home}/>
-     {/* <Route path='/products' exact component={Products}/>
-     <Route path='/contact' exact component={Contact}/>
-     <Route path='/about' exact component={About}/>
-
-     <Route path='/scarves' exact component={Scarves}/>
-     <Route path='/blankets' exact component={Blankets}/>
-     <Route path='/beanies' exact component={Beanies}/>
-     <Route path='/kandi' exact component={Kandi}/>
-     <Route path='/jewelry' exact component={Jewelry}/>
-     <Route path='/magnets' exact component={Magnets}/>
-     <Route path='/coasters' exact component={Coasters}/>
-     <Route path='/checkout' exact component={Checkout}/> */}
-
-     </BrowserRouter>
+      </header>
     </div>
   );
 }
