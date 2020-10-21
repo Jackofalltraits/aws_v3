7b50fef34755f8cc005048ae963901ba3418df15
jackkower <jackkower@gmail.com>
2020-10-21 02:53:00 -0700

    delta8

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { } from "react-router-dom";
+import Navigation from './Navigation';
+
+function Home() {
+  return (
+    <div className="home">
+    <Navigation/>
+
+    </div>
+  )
+}
+
+export default Home;
