b49b37f09291f79c08066dc695b16dc554813905
jackkower <jackkower@gmail.com>
2020-10-21 01:29:00 -0700

    form added

--- src/App.js
+++ src/App.js
@@ -6,18 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeSz3w3zob-fpsTzJUQ5l9uyn_5t-2kec1q2eAAahpbhceKWA/viewform?embedded=true" width="640" height="673" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+
       </header>
     </div>
   );
